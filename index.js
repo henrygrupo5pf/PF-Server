b2e2a0d18fbf8bd6a6be2fb53ed037f32a7e9777
ASalasAbaunza <alex.aba2409@gmail.com>
2024-01-22 21:26:31 -0600

    Database and models created

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log('Hello World');
-console.log('Hola Alejandro');
+const axios = require("axios");
+const server = require("./src/server");
+const conn = require('./src/db.js');
+
+const PORT = 3001;
+
+conn.sync({ force: true })
+  .then(async () => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    })
+  })
+  .catch(error => console.error(error))
