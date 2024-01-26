504a3ed3ea5b5eee53ba84785e5fa983fe4d6dc5
ASalasAbaunza <alex.aba2409@gmail.com>
2024-01-25 20:24:19 -0600

    Correction in filter

--- src/controllers/productControllers/getFilteredProducts.js
+++ src/controllers/productControllers/getFilteredProducts.js
@@ -4,10 +4,8 @@ const filterProducts = async (category, minCost, maxCost) => {
     const filterCriteria = {
         category,
         cost: {
-          [Sequelize.Op.and]: [
-            { [Sequelize.Op.gte]: minCost || 0 },
-            { [Sequelize.Op.lte]: maxCost || Number.MAX_SAFE_INTEGER },
-          ],
+            [Sequelize.Op.gte]: minCost || 0,
+            [Sequelize.Op.lte]: maxCost || Number.MAX_SAFE_INTEGER,
         },
     };
 
