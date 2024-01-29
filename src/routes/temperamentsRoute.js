630bd75f5d5586c8c40f6891875d03911c0f7193
Maharba07 <maharba1707@gmail.com>
2024-01-29 00:12:45 -0400

    PFHenry

--- src/routes/temperamentsRoute.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-//const { Driver, Team } = require('../db.js');
-const { getTemperamentsHandler } = require("../handlers/temperamentsHandlers");
-const temperamentsRouter = Router();
-temperamentsRouter.get("/", getTemperamentsHandler);
-
-module.exports = temperamentsRouter;
