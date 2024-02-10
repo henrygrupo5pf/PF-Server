9f19068ea1b6eaf9e7e6560b70732ec55d86fc35
Maharba07 <maharba1707@gmail.com>
2024-02-10 15:15:17 -0400

    Configuracion envio correo de notificacion

--- src/controllers/userControllers/postUser.js
+++ src/controllers/userControllers/postUser.js
@@ -1,7 +1,15 @@
 const { User } = require('../../db');
+const transporter = require('../../utils/mailer');
 
 const postUser = async({name, email, password, country, location, phoneNumber, activeStatus, admin})=>{
+    
     const newUser = await User.create({ name, email, password, country, location, phoneNumber, activeStatus, admin });
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER, // Dirección de correo electrónico que se mostrará como remitente
+        to: email, // Correo electrónico del nuevo usuario
+        subject: '¡Bienvenido!', // Asunto del correo
+        text: `Hola ${name},\n\nBienvenido a nuestra plataforma. Esperamos que disfrutes de nuestros servicios.\n\nSaludos,\nEl equipo de tu aplicación`, // Cuerpo del correo
+    });
 
     return newUser;
 
---
+++ src/utils/mailer.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+  });
+  module.exports = transporter;
