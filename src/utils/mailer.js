fd69511613ca20a0b4a9bab6f24ed8d5ea52966c
Abraham Garcia <126897616+Maharba07@users.noreply.github.com>
2024-02-16 01:30:04 -0400

    Update mailer.js

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,9 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
-    port: 587,
-    secure: false, // STARTTLS
+    service: "hotmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_ZPASS,
