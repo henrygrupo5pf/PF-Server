6e4e2215cfc7705644794cf698e3b8888508835a
Maharba07 <maharba1707@gmail.com>
2024-02-20 00:54:14 -0400

    Un cambio pa mi

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
     secure: false, // STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_ZPASS,
     }
 });
 
