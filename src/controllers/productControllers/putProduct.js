c772fb5b0a7b3bb91b879df2c806ad64b12e807c
harlock184 <105575194+harlock184@users.noreply.github.com>
2024-02-22 01:22:04 -0600

    modificando putProduct.js

--- src/controllers/productControllers/putProduct.js
+++ src/controllers/productControllers/putProduct.js
@@ -1,11 +1,55 @@
+// const { Product } = require('../../db');
+
+// const putProduct = async ({ id, cost, activeStatus, photo, name, description, category }) => {
+//     let updateFields = {};
+//     if (cost && cost >= 0) {
+//         updateFields.cost = cost;
+//     }
+//      if (typeof activeStatus !== 'undefined') {
+//         updateFields.activeStatus = activeStatus;
+//     }
+
+//     if(name){
+//         updateFields.name = name;
+//     }
+
+//     if(description){
+//         updateFields.description = description;
+//     }
+
+//     if(category){
+//         updateFields.category = category;
+//     }
+
+//     if(photo){
+//         updateFields.photo = photo;
+//     }
+
+//     if (Object.keys(updateFields).length === 0) {
+//         return {error: 'No valid fields to update.'};
+//     }
+
+//     const [updatedRowsCount] = await Product.update(updateFields, {
+//         where: { id: id },
+//     });
+
+//     if (updatedRowsCount > 0) {
+//         return {message: 'Product updated successfully.'};
+//     } else {
+//         return {error: 'Product not found.'};
+//     }
+// };
+
+// module.exports = putProduct;
 const { Product } = require('../../db');
 
 const putProduct = async ({ id, cost, activeStatus, photo, name, description, category }) => {
     let updateFields = {};
-    if (cost && cost >= 0) {
+    if (cost !== undefined && cost >= 0) {
         updateFields.cost = cost;
     }
-    if (activeStatus) {
+    // Corregido para manejar correctamente la activación y desactivación
+    if (typeof activeStatus !== 'undefined') {
         updateFields.activeStatus = activeStatus;
     }
 
@@ -29,14 +73,20 @@ const putProduct = async ({ id, cost, activeStatus, photo, name, description, ca
         return {error: 'No valid fields to update.'};
     }
 
-    const [updatedRowsCount] = await Product.update(updateFields, {
-        where: { id: id },
-    });
+    try {
+        const [updatedRowsCount, [updatedProduct]] = await Product.update(updateFields, {
+            where: { id: id },
+            returning: true, // Esta opción es específica de algunos dialectos en Sequelize como PostgreSQL
+        });
 
-    if (updatedRowsCount > 0) {
-        return {message: 'Product updated successfully.'};
-    } else {
-        return {error: 'Product not found.'};
+        if (updatedRowsCount > 0) {
+            return { message: 'Product updated successfully.', product: updatedProduct };
+        } else {
+            return { error: 'Product not found.' };
+        }
+    } catch (error) {
+        console.error('Error updating product:', error);
+        return { error: error.message };
     }
 };
 
