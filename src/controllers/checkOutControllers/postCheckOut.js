7c995580cd992a0273cf0c019b59fada8ece0925
Maharba07 <maharba1707@gmail.com>
2024-02-23 01:28:06 -0400

    Actualizacion Codigo notificacion de pago 3

--- src/controllers/checkOutControllers/postCheckOut.js
+++ src/controllers/checkOutControllers/postCheckOut.js
@@ -1,9 +1,22 @@
 const Stripe = require("stripe");
 require("dotenv").config();
-const { PRIVATE_KEY_STRIPE } = process.env;
+const transporter = require('../../utils/mailer');
+//const { PRIVATE_KEY_STRIPE } = process.env;
 
 const stripe = new Stripe("sk_test_51OgYb1GL3gYQY1hZDp1omCdXOyZlCpwMune57tHpXClYf6bYYCgXod6fis9dTOSzBqqDD9MqmGA1bZh5kGL9CXxf00OfwTLt23");
-
+const sendEmail = async (recipient, subject, message) => {
+  try {
+      await transporter.sendMail({
+          from: process.env.EMAIL_USER,
+          to: recipient,
+          subject: subject,
+          text: message,
+      });
+      console.log("Correo electrónico enviado con éxito a", recipient);
+  } catch (error) {
+      console.error("Error al enviar el correo electrónico:", error);
+  }
+};
 const checkOut = async (info) => {
 
 
@@ -40,7 +53,7 @@ const checkOut = async (info) => {
     /* success_url: "http://localhost:5173/checkout/success",
     cancel_url: "http://localhost:5173/checkout/cancel", */
   });
-
+  await sendEmail(recipient, "Pago exitoso", "¡Gracias por tu compra!");
 console.log("SOY CHECKOUT TERMINANDO:  ", session )
   return session 
 
