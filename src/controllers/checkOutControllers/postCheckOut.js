bddcdeabd275ac39e8a98163e11c4ee6e0b8d860
Maharba07 <maharba1707@gmail.com>
2024-02-23 04:48:14 -0400

    Actualizacion Codigo notificacion de pago 10

--- src/controllers/checkOutControllers/postCheckOut.js
+++ src/controllers/checkOutControllers/postCheckOut.js
@@ -1,9 +1,10 @@
 const Stripe = require("stripe");
 require("dotenv").config();
 const transporter = require('../../utils/mailer');
-//const { PRIVATE_KEY_STRIPE } = process.env;
+const { User } = require('../../db');
 
 const stripe = new Stripe("sk_test_51OgYb1GL3gYQY1hZDp1omCdXOyZlCpwMune57tHpXClYf6bYYCgXod6fis9dTOSzBqqDD9MqmGA1bZh5kGL9CXxf00OfwTLt23");
+
 const sendEmail = async (email, subject, message) => {
   try {
       await transporter.sendMail({
@@ -17,15 +18,13 @@ const sendEmail = async (email, subject, message) => {
       console.error("Error al enviar el correo electrónico:", error);
   }
 };
-const checkOut = async (info) => {
 
-
+const checkOutHandler = async (info) => {
   const lineItems = info.map((item) => {
-
     const startDate = new Date(item.startDate);
     const endDate = new Date(item.endDate);
     const timeDifference = Math.abs(endDate.getTime() - startDate.getTime());
-    const totalDays = Math.ceil(timeDifference / (1000 * 3600 * 24))+1
+    const totalDays = Math.ceil(timeDifference / (1000 * 3600 * 24)) + 1;
 
     return {
       price_data: {
@@ -34,13 +33,13 @@ const checkOut = async (info) => {
           description: item.description,
         },
         currency: "USD",
-        unit_amount: item.cost * 100 * totalDays 
+        unit_amount: item.cost * 100 * totalDays,
       },
       quantity: item.quantity,
     };
   });
 
-  console.log("SOYlineItems :  ", lineItems)
+  console.log("SOYlineItems :  ", lineItems);
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
@@ -48,21 +47,25 @@ const checkOut = async (info) => {
     mode: "payment",
     success_url: "https://pf-front-end.onrender.com/#/checkOut/success",
     cancel_url: "https://pf-front-end.onrender.com/#/checkOut/cancel",
-
-    //URL de prueba descomentar abajo
-    /* success_url: "http://localhost:5173/checkout/success",
-    cancel_url: "http://localhost:5173/checkout/cancel", */
   });
 
-console.log("SOY CHECKOUT TERMINANDO:  ", session )
+  console.log("SOY CHECKOUT TERMINANDO:  ", session);
 
-  // Enviar el correo electrónico si el pago fue exitoso
-  await sendEmail(info.email, "Pago exitoso", "¡Gracias por tu compra!");
+  // Buscar el usuario en la base de datos por su ID
+  try {
+    const user = await User.findByPk(info.userId);
+    if (!user) {
+      throw new Error("Usuario no encontrado en la base de datos");
+    }
+    // Enviar el correo electrónico si el pago fue exitoso
+    await sendEmail(user.email, "Pago exitoso", "¡Gracias por tu compra!");
+  } catch (error) {
+    console.error("Error al buscar el usuario en la base de datos:", error);
+  }
 
-  return session 
-
+  return session;
 };
 
 module.exports = {
-  checkOut,
+  checkOutHandler,
 };
